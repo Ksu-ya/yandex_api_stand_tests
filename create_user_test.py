c276f0816b7bf12c9e11ecda25dd49022e39cf84
kseniya <ksenya.mylnikova.93@mail.ru>
2024-02-25 02:59:01 +0500

    pravki 1, ispravlenny zaprosy k BD

--- create_user_test.py
+++ create_user_test.py
@@ -1,6 +1,6 @@
 import data
 import sender_stand_request
-
+# КСЕНИЯ ЯКОВЕНКО 13-Я КОГОРТА ДИПЛОМНЫЙ ПРОЕКТ. ИНЖИНЕР ПО ТЕСТИРОВАНИЮ ПЛЮС
 
 def get_user_body(first_name, lastName, address,metroStation, rentTime, deliveryDate):
     current_body = data.user_body.copy()
@@ -14,14 +14,15 @@ def get_user_body(first_name, lastName, address,metroStation, rentTime, delivery
 def positive_assert(first_name, lastName, address,metroStation, rentTime, deliveryDate):
     user_body = get_user_body(first_name, lastName, address, metroStation, rentTime, deliveryDate)
     user_response = sender_stand_request.post_new_order(user_body)
-    assert user_response.status_code == 201
-    assert user_response.json()["track"] != ""
-    orders_trakc_response=sender_stand_request.post_new_order(user_body)
-    response=sender_stand_request.post_orders_track(user_response.json()["track"])
-    assert response.status_code == 200
+    assert user_response.status_code == 201 # здесь проверяем что заказ создан
+    assert user_response.json()["track"] != "" # и у него есть трек
+    orders_trakc_response=sender_stand_request.post_new_order(user_body) # записываем трек в переменную
+    response=sender_stand_request.post_orders_track(user_response.json()["track"]) # подставляем полученный трек в функцию полученя заказа по треку post_order_treck и записываем ответ
+    assert response.status_code == 200 # проверяем что статус ответа 200
 
 
 
 
-def test_poluchenie_zakaza_po_trecu():
+
+def test_poluchenie_zakaza_po_trecu(): # тест выполняет проверку создавая заказ с моими данными и подставляя его трек в функцию получения заказа
     positive_assert("Ксения", "Яковенко", "Ленина д.1", 12, 15, "2020-06-06")
--- sender_stand_request.py
+++ sender_stand_request.py
@@ -7,14 +7,7 @@ def post_new_order(body):
     return requests.post(configuration.URL_SERVICE + configuration.ORDERS_PATH,  # подставляем полный url
                          json=body )
 
-# = post_new_order(data.user_body);
-#print(response.status_code, response.json())
-#print({"t": response.json()["track"]})
-#print(response.url)
 def post_orders_track(track):
     return requests.post(configuration.URL_SERVICE + configuration.ORDERS_TRACK_PATH,
                         params={"t": track } )
 
-#response = post_orders_track();
-#print(response.url)
-#print(response.status_code, response.json())
